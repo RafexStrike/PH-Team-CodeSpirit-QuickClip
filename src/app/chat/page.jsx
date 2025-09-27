9b88b9c7ef375bbc3119e0e9f64f7b5662686c2f
rafexstrike <adnanrahmanrafi515@gmail.com>
2025-09-27 23:13:17 +0600

    each chat session now have a unique ID.

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -6,6 +6,8 @@ import ChatWindow from "@/components/chat/ChatWindow";
 import ChatComposer from "@/components/chat/ChatComposer";
 
 export default function ChatPage() {
+  const [chatUniqueID, setChatUniqueID] = useState(Date.now());
+ 
   const [messages, setMessages] = useState([
     {
       id: 1,
@@ -16,7 +18,7 @@ export default function ChatPage() {
   ]);
   const [inputValue, setInputValue] = useState("");
 
-  // caution starts : trying to send the data to the api to save to mongodb
+
   const saveChatDataToDatabase = async (data) => {
     try {
       const dataToSend = await fetch("/api/getAndPostChatData", {
@@ -33,20 +35,20 @@ export default function ChatPage() {
       );
     }
   };
-  // caution ends
+
 
   // normal text message send
   const handleSendMessage = async (message) => {
     if (!message.trim()) return;
     const newUserMessage = {
-      id: Date.now(),
+      id: chatUniqueID,
       content: message,
       sender: "user",
       timestamp: new Date(),
     };
-    // caution starts : trying to send the data to the api to save to mongodb
+ 
     saveChatDataToDatabase(newUserMessage);
-    // caution ends
+
     setMessages((prev) => [...prev, newUserMessage]);
     setInputValue("");
     try {
@@ -56,21 +58,19 @@ export default function ChatPage() {
         body: JSON.stringify({ prompt: message }),
       });
       const data = await res.json();
-      // caution starts : trying to send the data to the api to save to mongodb
       const newBotMessage = {
-        id: Date.now() + 1,
+        id: chatUniqueID,
         content: data.reply || "No response",
         sender: "bot",
         timestamp: new Date(),
       };
       saveChatDataToDatabase(newBotMessage);
-      // caution ends
       setMessages((prev) => [...prev, newBotMessage]);
     } catch (err) {
       setMessages((prev) => [
         ...prev,
         {
-          id: Date.now() + 1,
+          id: chatUniqueID,
           content: "Error contacting AI: " + err.message,
           sender: "bot",
           timestamp: new Date(),
@@ -85,7 +85,9 @@ export default function ChatPage() {
         conversations={[]}
         activeConversation={1}
         onConversationSelect={() => {}}
-        onNewChat={() => {}}
+        onNewChat={() => {
+          setChatUniqueID(Date.now());
+        }}
       />
       <div className="flex flex-col flex-1">
         <ChatWindow messages={messages} />
