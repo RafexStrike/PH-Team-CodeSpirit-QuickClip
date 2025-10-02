b61bc9dbc3f4cce72184761b538269a1c3aca0b7
rafexstrike <adnanrahmanrafi515@gmail.com>
2025-10-02 19:40:47 +0600

    updated eslint.config.mjs for fixing vercel issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,9 +19,15 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-    rules: {
-     
-    },
+    overrides: [
+      {
+        files: ["*.js", "*.jsx"], // apply only to JS files
+        rules: {
+          "@typescript-eslint/no-explicit-any": "off", // no TS rules in JS
+          "@typescript-eslint/*": "off", // turn off ALL TS rules
+        },
+      },
+    ],
   },
 ];
 
