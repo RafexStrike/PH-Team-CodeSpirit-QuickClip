7051f966600cd3332f9b5c31067a2abef4d770c9
rafexstrike <adnanrahmanrafi515@gmail.com>
2025-10-05 09:12:44 +0600

    all the chat responses of a specific chat session by bot are now shown in the console

--- src/app/api/chatsViaUniqueID/route.js
+++ src/app/api/chatsViaUniqueID/route.js
@@ -3,17 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const client = await clientPromise;
-  const database = client.db("ChatsDataRafi");
-  const allTheNotesWithUniqueID = await database.collection("ChatsData");
+  const database = client.db("ChatDataRafi");
+  const ChatsData = await database.collection("ChatsData");
 
-  // --- get the query parameter ?id=1759123536081 
+  // --- get the query parameter ?id=1759123536081
   const { searchParams } = new URL(request.url);
   const sessionId = Number(searchParams.get("id")); // convert from string → number
 
-  // --- find all chats with same id and sender = "bot" 
-  const chats = await chatsCollection
-    .find({ id: sessionId, sender: "bot" })
-    .toArray();
+  // --- find all chats with same id and sender = "bot"
+  const chats = await ChatsData.find({
+    id: sessionId,
+    sender: "bot",
+  }).toArray();
 
   return NextResponse.json(chats);
 }
